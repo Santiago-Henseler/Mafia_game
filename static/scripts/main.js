6c5421815fc996ed84fa5014624bd19aeb6ff383
Santiago-Henseler <116313638+Santiago-Henseler@users.noreply.github.com>
2025-11-11 20:11:44 -0300

    cambio para que corra sobre tls

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,7 +1,7 @@
 const IP = "mafia-game-p4qe.onrender.com";
 const PUERTO = 4000;
 const WEB_URL = `https://${IP}:${PUERTO}`;
-const WS_URL = `ws://${IP}:${PUERTO}`;
+const WS_URL = `wss://${IP}:${PUERTO}`;
 
 let roomId = null;
 let socket = null;
