5abf5386c03f41ded1bc7720f66d7462a6ac80d2
Santiago-Henseler <116313638+Santiago-Henseler@users.noreply.github.com>
2025-11-12 14:26:08 -0300

    change IP

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,5 +1,5 @@
-const IP = "mafia-game-p4qe.onrender.com";
-const PUERTO = 443;
+const IP = "52.14.112.59";
+const PUERTO = 4000;
 const WEB_URL = `https://${IP}:${PUERTO}`;
 const WS_URL = `wss://${IP}:${PUERTO}`;
 
